85d9ba2159a1a6312cc4921fad07f48b5f954a66
superzhaoyang <1599602677@qq.com>
2019-09-12 16:58:31 +0800

    Site updated: 2019-09-12 16:58:28

--- dist/music.js
+++ dist/music.js
@@ -4,10 +4,16 @@ const ap = new APlayer({
     autoplay: false,
     audio: [
       {
-        name: "蜂鸟",
+        name: "太空",
         artist: '吴青峰',
-        url: 'http://www.ytmp3.cn/down/60648.mp3',
-        cover: 'http://oeff2vktt.bkt.clouddn.com/image/84.jpg',
+        url: '/1.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
+      },
+      {
+        name: 'May i see you again',
+        artist: 'MT1990',
+        url: 'http://m10.music.126.net/20190911235304/9f4f6b0a6b3caa1d79c7b7e187f1a7bd/ymusic/5d63/c894/9987/1401720c826f71e95d3a9d296008782f.mp3',
+        cover: 'http://oeff2vktt.bkt.clouddn.com/image/96.jpg',
       }
     ]
 });
